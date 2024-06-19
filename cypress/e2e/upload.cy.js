04969d641a41f9cb548e5652ddd842695b1f55a3
MKim <mrn.kim07@gmail.com>
2024-06-19 11:04:03 +0900

    Stop tracking cypress.config.js, /cypress and /.nyc_output

--- cypress/e2e/upload.cy.js
+++
@@ -1,8 +0,0 @@
-import { toBeDisabled } from "@testing-library/jest-dom/matchers";
-
-describe("Upload", () => {
-  it("successfully shows the upload button is initially disabled", () => {
-    cy.visit("/");
-    cy.get('[data-cy="data-cy-upload-button"]').should("be.disabled");
-  });
-});
